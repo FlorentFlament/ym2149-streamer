ff501af516e460d857a4d92893108ae9984f4277
Sebastien Delestaing <sebastien.delestaing@gmail.com>
2018-04-15 17:14:57 +0200

    Add sample song

--- main.c
+++ main.c
@@ -7,15 +7,16 @@ void set_led_out(void) {
 }
 
 void clear_registers(void) {
-  int i;
-  for (i=0; i<14; i++) {
-    send_data(i, 0);
-  }
+  send_data(6, 0);
+  send_data(7, 0x80);
+  send_data(9, 0);
+  send_data(10, 0);
 }
 
 int main() {
   unsigned int i;
   unsigned char data[14];
+  unsigned char cban_leds;
 
   set_ym_clock();
   set_bus_ctl();
@@ -27,15 +28,19 @@ int main() {
     for (i=0; i<14; i++) {
       data[i] = getByte();
     }
+
+    // Set IOA port as output
+    data[7] |= 0x80;
+
     // Do not write r13 if it equals 0xff (prevent enveloppe reset)
     set_registers(data, data[13] == 0xff ? 12 : 13);
 
     // Have LED blink with noise (drums)
-    if (~data[7] & 0x38) {
-      PORTB |=   1 << LED;
-    } else {
-      PORTB &= ~(1 << LED);
-    }
+    cban_leds = (~data[7] & 0x38) ? 0x01 : 0x00;
+    // Add ABC volume LEDs
+    cban_leds |= ((data[8] & 0x08) >> 2) | ((data[9] & 0x08) >> 1) | (data[10] & 0x08);
+    // Set IOA
+    send_data(14, cban_leds);
   }
 
   return 0;
