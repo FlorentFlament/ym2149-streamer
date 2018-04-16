8ec259ab7579d6a4ea6eb8b824edc1c67d30c4e7
Sebastien Delestaing <sebastien.delestaing@gmail.com>
2018-04-16 21:30:55 +0200

    remove digidrums for now and fix portA setup

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 
 void clear_registers(void)
 {
-  send_data(6, 0);
-  send_data(7, 0x80);
-  send_data(9, 0);
-  send_data(10, 0);
+  unsigned char rclear[] = {0, 0, 0, 0, 0, 0, 0, 0x40,
+                           0, 0, 0, 0, 0, 0, 0, 0};
+
+  set_registers(rclear, 16);
 }
 
 int main()
@@ -15,12 +15,13 @@ int main()
   unsigned int i;
   unsigned char r[16];
   unsigned char cban_leds;
-  unsigned char dd_cmd, dd_sample;
+  // unsigned char dd_cmd, dd_sample;
 
   set_ym_clock();
   set_bus_ctl();
   initUART();
-  fx_setupTimer();
+  //fx_setupTimer();
+
   clear_registers();
 
   for/*ever*/(;;)
@@ -30,30 +31,30 @@ int main()
       r[i] = getByte();
     }
 
-    // r3 free bits are used to code a DD start.
-    // r3 b5-b4 is a 2bits code wich means:
-    dd_cmd = (r[3] & 0x30) >> 4;
+    // // r3 free bits are used to code a DD start.
+    // // r3 b5-b4 is a 2bits code wich means:
+    // dd_cmd = (r[3] & 0x30) >> 4;
 
-    // 00:     No DD
-    // 01:     DD starts on voice A
-    // 10:     DD starts on voice B
-    // 11:     DD starts on voice C
-    if (dd_cmd)
-    {
-      const char voice[4] = {0, 8, 9, 10};
-      char dd_voice = voice[dd_cmd];
+    // // 00:     No DD
+    // // 01:     DD starts on voice A
+    // // 10:     DD starts on voice B
+    // // 11:     DD starts on voice C
+    // if (dd_cmd)
+    // {
+    //   const char voice[4] = {0, 8, 9, 10};
+    //   char dd_voice = voice[dd_cmd];
 
-      // WARNING:
-      // If a DD starts on voice V, the volume register corresponding to V (Ex r8 for voice A,
-      // r9 for B and r10 for C) contains the sample number in 5 low bits (That mean you have
-      // 32 digiDrum max in a song).
-      dd_sample = r[(int)dd_voice] & 0x1f;
+    //   // WARNING:
+    //   // If a DD starts on voice V, the volume register corresponding to V (Ex r8 for voice A,
+    //   // r9 for B and r10 for C) contains the sample number in 5 low bits (That mean you have
+    //   // 32 digiDrum max in a song).
+    //   dd_sample = r[(int)dd_voice] & 0x1f;
 
-      fx_playDigidrum(dd_sample, dd_voice);
-    }
+    //   fx_playDigidrum(dd_sample, dd_voice);
+    // }
 
     // Set IOA port as output
-    r[7] |= 0x80;
+    r[7] |= 0x40;
 
     // As we've seen, r13 has a particular status. If the value stored in the file is 0xff,
     // YM emulator will not reset the waveform position.
